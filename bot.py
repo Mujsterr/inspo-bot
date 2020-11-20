250fa9fe8c6065f74be98d806450141bd9feb33d
mujtabaAhmed <m-10j@hotmail.com>
2020-11-20 18:40:31 -0500

    Added >inspo command to bot

--- bot.py
+++ bot.py
@@ -5,8 +5,16 @@ import scraper
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv('TOKEN')
 IMG_LINK = scraper.img_url_grabber()
+ 
+
 client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.find(">inpso") != -1:
+        await message.channel.send(IMG_LINK)
 
 client.run(TOKEN)
