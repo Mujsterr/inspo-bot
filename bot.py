1f6180ebf31ec5e8e08b00e49d7007c95b534471
mujtabaAhmed <m-10j@hotmail.com>
2020-11-20 22:10:23 -0500

    added on ready event

--- bot.py
+++ bot.py
@@ -7,15 +7,20 @@ from discord.ext import commands
 
 
 TOKEN = os.environ.get("TOKEN")
-IMG_LINK = scraper.img_url_grabber()
-
+#IMG_LINK = scraper.img_url_grabber()
+client = commands.Bot(command_prefix= '>')
 
 client = discord.Client()
 
 
 @client.event
+async def on_ready():
+    print("Hello!")
+
+
+@client.event
 async def on_message(message):
     if message.content == ">inpso":
-        await message.channel.send("HellO!")
+        await message.channel.send("Hello!")
 
 client.run(TOKEN)
