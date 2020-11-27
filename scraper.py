7fb8c49dcc0b905ae0a790e3a0889c96d45b12e8
mujtabaAhmed <m-10j@hotmail.com>
2020-11-27 16:40:49 -0500

    updated explicit wait times

--- scraper.py
+++ scraper.py
@@ -13,8 +13,8 @@ def img_url_grabber():
     IMG_XPATH = '//*[@id="top"]/div[1]/div[1]/img'
     BTN_XPATH = '/html/body/div[2]/div[1]/div[1]/div[2]/div'
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--start-maximized')
     chrome_options.add_argument('--disable-gpu')
@@ -25,8 +25,8 @@ def img_url_grabber():
     driver = webdriver.Chrome(options = chrome_options, executable_path = os.environ.get("CHROMEDRIVER_PATH"))
     driver.get('https://www.inspirobot.me')
 
-    WebDriverWait(driver, 10, 0.5).until(EC.element_to_be_clickable((By.XPATH, BTN_XPATH))).click()
-    image_src = WebDriverWait(driver, 10, 0.5).until(EC.visibility_of_element_located((By.XPATH, IMG_XPATH))).get_attribute('src')
+    WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, BTN_XPATH))).click()
+    image_src = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, IMG_XPATH))).get_attribute('src')
      
     driver.quit()
     
