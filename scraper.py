0486e85412e8eb8c7f562fc7cf6640ab306f8abf
mujtabaAhmed <m-10j@hotmail.com>
2020-11-28 01:28:21 -0500

    changed chrome_arguments

--- scraper.py
+++ scraper.py
@@ -10,12 +10,12 @@ from selenium.webdriver.common.by import By
 from selenium.common import exceptions
 
 def img_url_grabber():
-    IMG_XPATH = '//*[@id="top"]/div[1]/div[1]/img'
-    BTN_XPATH = '/html/body/div[2]/div[1]/div[1]/div[2]/div'
     chrome_options = Options()
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument('--single-process')
     chrome_options.add_argument('--start-maximized')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-extensions')    
@@ -24,8 +24,8 @@ def img_url_grabber():
     driver = webdriver.Chrome(options = chrome_options, executable_path = os.environ.get("CHROMEDRIVER_PATH"))
     driver.get('https://www.inspirobot.me')
 
-    WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, BTN_XPATH))).click()
-    image_src = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, IMG_XPATH))).get_attribute('src')
+    WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/div'))).click()
+    image_src = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="top"]/div[1]/div[1]/img'))).get_attribute('src')
      
     driver.quit()
     
