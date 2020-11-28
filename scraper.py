2e5fd5d8684a944b6c2cf68dd2d6133a7ddded1a
mujtabaAhmed <m-10j@hotmail.com>
2020-11-28 01:57:10 -0500

    Updated explicit wait times

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common import exceptions
+
 
 def img_url_grabber():
     chrome_options = Options()
@@ -24,8 +24,8 @@ def img_url_grabber():
     driver = webdriver.Chrome(options = chrome_options, executable_path = os.environ.get("CHROMEDRIVER_PATH"))
     driver.get('https://www.inspirobot.me')
 
-    WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/div'))).click()
-    image_src = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="top"]/div[1]/div[1]/img'))).get_attribute('src')
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/div'))).click()
+    image_src = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="top"]/div[1]/div[1]/img'))).get_attribute('src')
      
     driver.quit()
     
